37317afc0343ac816380cd38162a98a12b4d1212
AshCherr96 <ashleycherry16@gmail.com>
2026-04-07 13:45:16 -0500

    completed lesson 1

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,22 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Ashley';
+  const age = 30;
+  const hobbies = ['Coding', 'Reading', 'Watching Movies', 'Gaming'];
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h2>About Me</h2>
+      <p>
+        Hello, my name is {name} and I am {age} years old.
+      </p>
+
+      <h3>My Hobbies</h3>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
