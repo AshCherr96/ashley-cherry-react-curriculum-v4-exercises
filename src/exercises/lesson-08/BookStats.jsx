692e1340754460031a3249569baf19ee5481dd58
AshCherr96 <ashleycherry16@gmail.com>
2026-05-26 13:56:28 -0500

    Optimized Book Dashboard with useMemo and useCallback

--- src/exercises/lesson-08/BookStats.jsx
+++ src/exercises/lesson-08/BookStats.jsx
@@ -1,3 +1,4 @@
+import React, { useMemo } from 'react';
 import {
   useRenderCounter,
   RenderCounter,
@@ -89,7 +90,9 @@ function BookStats({ books }) {
     };
   };
 
-  const stats = calculateStats();
+  const stats = useMemo(() => {
+    return calculateStats(books);
+  }, [books]);
 
   return (
     <div className={styles.statsContainer}>
